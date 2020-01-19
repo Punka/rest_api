e13499761c7da74dc770912027805d34d52f54dc
punka86 <pun321456>
2020-01-19 10:28:26 +0500

    init project

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => '\yii\mongodb\Connection',
+    'dsn' => 'mongodb://@localhost:27017/test',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
